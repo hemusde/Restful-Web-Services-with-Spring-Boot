af04a917ef175f96380fed117f637d2bbe80a917
hemusde <hemanth.sde@gmail.com>
2023-03-24 23:16:38 +0530

    Create Hello World REST API Controller

---
+++ src/main/java/com/hemu/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.hemu.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello-world")
+	public String helloworld() {
+		return "Hello World";
+	}
+
+}
