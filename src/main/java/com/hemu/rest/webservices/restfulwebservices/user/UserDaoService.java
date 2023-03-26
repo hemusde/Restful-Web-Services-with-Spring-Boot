e488269f27eb46dfe71ecd9dacbde7888d8e832f
hemusde <hemanth.sde@gmail.com>
2023-03-27 00:11:16 +0530

    Add a Post Method

--- src/main/java/com/hemu/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/hemu/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -12,15 +12,23 @@ public class UserDaoService {
 
 	private static List<User> users = new ArrayList<>();
 
+	private static int UsersCount = 0;
+
 	static {
-		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
-		users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+		users.add(new User(++UsersCount, "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(++UsersCount, "Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(++UsersCount, "Jim", LocalDate.now().minusYears(20)));
 
 	}
 
 	public List<User> findAll() {
 		return users;
+	}
+
+	public User save(User user) {
+		user.setId(++UsersCount);
+		users.add(user);
+		return user;
 	}
 
 	public User findOne(int id) {
