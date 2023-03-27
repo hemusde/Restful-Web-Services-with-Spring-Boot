311215f8ed5d49386ab2dec5887b3cd51ff2d05d
hemusde <hemanth.sde@gmail.com>
2023-03-27 14:16:18 +0530

    Add Validations

--- src/main/java/com/hemu/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/hemu/rest/webservices/restfulwebservices/user/User.java
@@ -2,10 +2,17 @@ package com.hemu.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
+
+	@Size(min = 2, message = "Name should have atleast 2 characters")
 	private String name;
+
+	@Past(message = "Birthdate should be in the past")
 	private LocalDate BirthDate;
 
 	public User() {
