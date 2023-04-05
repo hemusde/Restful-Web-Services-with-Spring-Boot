79ddae271032aa666566b21a1901d1e09e84dc5a
hemusde <hemanth.sde@gmail.com>
2023-04-05 19:19:05 +0530

    Add ManytoOne Relationship with User Entity

--- src/main/java/com/hemu/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/hemu/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,14 @@
 package com.hemu.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -24,6 +26,10 @@ public class User {
 	@Past(message = "Birthdate should be in the past")
 //	@JsonProperty("birth_date")
 	private LocalDate BirthDate;
+
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
 
 	protected User() {
 		super();
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,12 @@ insert into user_details(id, birth_date,name)
 values(10002,current_date(),'Romeo');
 insert into user_details(id, birth_date,name)
 values(10003,current_date(),'Sam');
+
+insert into post(id, description, user_id)
+values(20001,'I Want to Learn AWS',10001);
+insert into post(id, description, user_id)
+values(20002,'I Want to Learn Devops',10001);
+insert into post(id, description, user_id)
+values(20003,'I Want to get AWS certificate',10001);
+insert into post(id, description, user_id)
+values(20004,'I Want to Learn MUlticloud',10002);
